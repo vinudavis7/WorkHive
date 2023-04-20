9a0d1da910c3ea1f7fc50646e8f7d9d53c980f28
LAPTOP-I8RGMAV0\Admin <darshanshah4815@gmail.com>
2023-04-20 20:00:00 +0100

    Category service file added

---
+++ WorkHiveApi/BLL/CategoryService.cs
@@ -0,0 +1,34 @@
+﻿using BLL.Interface;
+using DAL.Repository;
+using DAL.Repository.Interface;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<Categories> GetCategories()
+        {
+            try
+            {
+                return _categoryRepository.GetCategories();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
